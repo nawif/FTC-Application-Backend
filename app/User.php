e8df9ae4b7d0833e481f4981e6706cc70c2fdc99
nawif <nnawif@gmail.com>
2019-06-12 16:18:58 +0300

    added required changes to apply jwt auth

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -37,6 +37,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     public function points()
     {
         return $this->hasMany('App\Point');
