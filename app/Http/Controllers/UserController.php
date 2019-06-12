0d3d59833b0c33d430c151d185ac54fa24cc2521
nawif <nnawif@gmail.com>
2019-06-12 17:37:25 +0300

    created store user function

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Http\Requests\StoreUser;
+use Illuminate\Auth\Access\Response;
+use App\TotalPoints;
+
+class UserController extends Controller
+{
+
+    public function store(StoreUser $request) {
+        $validatedData = $request->validated(); // if input doesn't meet the set rules a error message will be thrown, otherwise it will continue
+
+        $newUser = new User($validatedData);
+        $newUser->password = $validatedData['password']; //because password is hidden.
+        $newUser->profilephoto = $newUser->getDefaultPhoto();
+
+        $newUser->save();
+
+        $newUserPoints = new TotalPoints(); //creating a record in the TotalPoints table and attaching it to the user.
+        $newUserPoints->user_id=$newUser->id;
+
+        $newUserPoints->save();
+
+        return Response(["message" => "user created successfully"], 201);
+
+    }
+}
