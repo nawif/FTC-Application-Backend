147ed46d42944d06779ef235d60887b5f7fee064
nawif <nnawif@gmail.com>
2019-06-13 22:59:33 +0300

    added task resource

---
+++ app/Http/Resources/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Task extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'description' => $this->description,
+            'date' => $this->created_at
+        ];
+    }
+}
