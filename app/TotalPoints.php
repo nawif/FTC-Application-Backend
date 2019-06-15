4445f6f9d12cde33bdaf37d54ccc83f220a53669
nawif <nnawif@gmail.com>
2019-06-15 13:19:07 +0300

    added vlaue to model

--- app/TotalPoints.php
+++ app/TotalPoints.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TotalPoints extends Model
 {
     protected $fillable = [
-        'user_id'
+        'user_id', 'value',
     ];
 
     public function user()
