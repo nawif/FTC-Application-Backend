2228f5cdc3f627c6ae0e22c11a51e888b2d92047
nawif <nnawif@gmail.com>
2019-06-28 16:08:07 +0300

    implemented the user service interface

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Service\UserService\UserServiceImp;
+
+// use App\Service\UserService\UserService;
 
 class UserServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,12 @@ class UserServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        $this->app->bind(
+            'App\Service\UserService\UserServiceInterface', function ($app) {
+                return new UserServiceImp();
+              }
+        );
+        // $this->app->bind('App\Service\UserService\UserServiceInterface', 'App\Service\UserService\UserServiceImp');
     }
 }
---
+++ app/Service/UserService/UserServiceImp.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Service\UserService;
+
+use App\Service\UserService\UserServiceInterface;
+use App\TotalPoints;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserServiceImp implements UserServiceInterface{
+
+    public function store($user_data)
+    {
+        $newUser = new User($user_data);
+        $newUser->password = Hash::make($user_data['password']);
+        $newUser->profilephoto = $newUser->getDefaultPhoto();
+
+        $newUser->save();
+
+        $newUserPoints = new TotalPoints(); //creating a record in the TotalPoints table and attaching it to the user.
+        $newUserPoints->user_id=$newUser->id;
+        $newUserPoints->save();
+    }
+}
