93b7f8bb08d0a775cb6180ce3c665fca0ee9bdeb
nawif <nnawif@gmail.com>
2019-06-28 16:07:51 +0300

    removed user controller logic to service

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,34 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
+use App\Service\UserService\UserServiceInterface;
 use App\Http\Requests\StoreUser;
 use Illuminate\Auth\Access\Response;
-use App\TotalPoints;
-use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
 
-    public function store(StoreUser $request) {
+    public function store(StoreUser $request, UserServiceInterface $userService ) {
         $validatedData = $request->validated(); // if input doesn't meet the set rules a error message will be thrown, otherwise it will continue
-
-        
-        $newUser = new User($validatedData);
-        $newUser->password = Hash::make($validatedData['password']);
-        $newUser->profilephoto = $newUser->getDefaultPhoto();
-
-        $newUser->save();
-
-        $newUserPoints = new TotalPoints(); //creating a record in the TotalPoints table and attaching it to the user.
-        $newUserPoints->user_id=$newUser->id;
-
-        $newUserPoints->save();
-
+        $userService->store($validatedData);
         return Response(["message" => "user created successfully"], 201);
     }
-
-
 
 }
