bcf1edcfc7dd2dba49b89dc4e114da178fc613c7
nawif <nnawif@gmail.com>
2019-06-28 18:07:03 +0300

    added registered users validation to store event request

--- app/Http/Requests/StoreEvent.php
+++ app/Http/Requests/StoreEvent.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class StoreEvent extends FormRequest
 {
@@ -16,12 +19,10 @@ class StoreEvent extends FormRequest
     {
         return [
             'name' => 'required',
-            'active_url' => 'required',
-            'leader_id' => 'required|exists:users,id',
             'description' => 'required|min:15|max:200',
             'user_limit' => 'required',
-            'date' => 'required',
             'type' => ['required', Rule::in(['ORGANIZE', 'ATTEND'])],
+            'registered_users.*' => 'exists:users,id'
         ];
     }
 
