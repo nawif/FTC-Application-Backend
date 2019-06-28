a4e988fd5ab9d6ef83eb5ca7652cbd65efbbf7e7
nawif <nnawif@gmail.com>
2019-06-28 16:25:06 +0300

    added default whatsapp social media account creation

--- app/Service/UserService/UserService.php
+++ app/Service/UserService/UserService.php
@@ -5,6 +5,7 @@ use App\Service\UserService\UserServiceContract;
 use App\TotalPoints;
 use Illuminate\Support\Facades\Hash;
 use App\User;
+use App\SocialmediaAccount;
 
 class UserService implements UserServiceContract{
 
@@ -19,5 +20,12 @@ class UserService implements UserServiceContract{
         $newUserPoints = new TotalPoints(); //creating a record in the TotalPoints table and attaching it to the user.
         $newUserPoints->user_id=$newUser->id;
         $newUserPoints->save();
+
+        $defaultWhatsappNumber = new SocialmediaAccount([
+            'platform'=>'whatsapp',
+            'username'=>$newUser->phone,
+            'user_id'=>$newUser->id,
+        ]);
+        $defaultWhatsappNumber->save();
     }
 }
--- app/SocialmediaAccount.php
+++ app/SocialmediaAccount.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialmediaAccount extends Model
 {
+    protected $socialMediaPlatforms = ['twitter', 'steam', 'whatsapp', 'linkedin', 'snapchat'];
+
     protected $fillable = [
         'platform', 'username', 'user_id'
     ];
