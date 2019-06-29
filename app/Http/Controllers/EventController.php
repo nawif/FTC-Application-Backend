fad39152cf28febd856757aa3bef9fa65367bc0a
nawif <nnawif@gmail.com>
2019-06-29 20:04:45 +0300

    implemented the get event details in controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreEvent;
 use App\Service\EventService\EventServiceContract;
+use App\Event;
+use App\Http\Resources\EventDetails as EventDetailsResources;
+
 
 class EventController extends Controller
 {
-    public function store(StoreEvent $request, EventServiceContract $eventService )
-    {
+    public function store(StoreEvent $request, EventServiceContract $eventService ) {
         $validatedData = $request->validated();
         $event = $eventService->store($validatedData);
         return Response(["message" => "event ".$event->name." created successfully"], 201);
     }
 
-    public function getEvents(EventServiceContract $eventService)
-    {
+    public function getEvents(EventServiceContract $eventService) {
         $eventsResource = $eventService->getEvents();
         return Response($eventsResource, 200);
+    }
+
+    public function getEventDetails($id) {
+        try{
+            $event = Event::findOrfail($id);
+        } catch (\Throwable $th) {
+            return Response(['message' => "event doesn't exist"], 422);
+        }
+        return Response(new EventDetailsResources($event), 200);
+    }
+
+    public function enrollInEvent($id, EventServiceContract $eventService) {
+        $eventService->enrollInEvent($id);
     }
 }
