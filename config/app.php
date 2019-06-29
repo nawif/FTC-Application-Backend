e92461a82121cc9bebdf0027f62ab862e5e78149
nawif <nnawif@gmail.com>
2019-06-29 17:58:06 +0300

    added event service provider to the providers stack

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\UserServiceProvider::class,
+        App\Providers\UserEventsServiceProvider::class,
 
 
     ],
