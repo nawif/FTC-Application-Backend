297de198dbfd07ab0b96a3ba3b5474545fdc5107
nawif <nnawif@gmail.com>
2019-07-06 17:04:42 +0300

    added archive and patch event functions

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\StoreEvent;
 use App\Service\EventService\EventServiceContract;
 use App\Event;
 use App\Http\Resources\EventDetails as EventDetailsResources;
-
+use App\Http\Requests\PatchEvent;
 
 class EventController extends Controller
 {
@@ -32,6 +32,17 @@ class EventController extends Controller
 
     public function enrollInEvent($id, EventServiceContract $eventService) {
         $response = $eventService->enrollInEvent($id);
+        return $response;
+    }
+
+    public function patchEvent(PatchEvent $request, EventServiceContract $eventService ) {
+        $validatedData = $request->validated();
+        $response = $eventService->patchEvent($validatedData);
+        return $response;
+    }
+
+    public function archiveEvent($id, EventServiceContract $eventService ) {
+        $response = $eventService->archiveEvent($id);
         return $response;
     }
 }
